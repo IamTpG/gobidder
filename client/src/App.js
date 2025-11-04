7242ea21256ff048074cc5388331c048769710df
ThangTo <tothang141020@gmail.com>
2025-11-04 20:51:58 +0700

    chore: fix tailwindcss with version 3.4.1

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
-function App() {
+const App = () => {
   return (
     <Router>
+      <Header />
       <Routes>
-        <Route path="/" element={<div>Home</div>} />
+        <Route path="/" element={<h1>Home</h1>} />
       </Routes>
+      <Footer />
     </Router>
   );
-}
+};
 
 export default App;
