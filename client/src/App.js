4a6faee2c59d096c5be7ef6673585c9b35856d2f
ThangTo <tothang141020@gmail.com>
2025-11-04 20:09:34 +0700

    chore(common): Set up tailwindcss, react-router-dom and husky

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,13 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
-    <div></div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+      </Routes>
+    </Router>
   );
 }
 
