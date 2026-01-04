3dda765790342593ab3a617151e4af871babc902
dinosauce-285 <thanhbualqt@gmail.com>
2026-01-04 20:15:24 +0700

    fix(client): right way to receive message

--- client/src/contexts/AuthContext.jsx
+++ client/src/contexts/AuthContext.jsx
@@ -97,7 +97,17 @@ export const AuthProvider = ({ children }) => {
       setUser(data);
       return data;
     } catch (error) {
-      let errorMsg = error.response?.data?.message || "Unknown login error.";
+      let errorMsg = "Unknown login error.";
+      if (error.response) {
+        if (typeof error.response.data === "string") {
+          errorMsg = error.response.data;
+        } else if (error.response.data?.message) {
+          errorMsg = error.response.data.message;
+        } else {
+          errorMsg = error.response.statusText || "Unknown login error.";
+        }
+      }
+
       if (errorMsg === "Unauthorized")
         errorMsg = "Incorrect email or password.";
       throw new Error(errorMsg);
